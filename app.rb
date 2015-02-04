78e2c22e2a3668ac334f913c290e1efcc90a8daa
Dmitry Zhuk <zhuchara@gmail.com>
2015-02-04 23:01:19 +0200

    Added index and new.

--- app.rb
+++ app.rb
@@ -5,4 +5,25 @@ set :database, 'sqlite3:todo.db'
 class Item < ActiveRecord::Base
 end
 
+get '/' do
+  @items = Item.order('created_at desc')
+
+  haml :index
+end
+
+get '/new' do
+  @item = Item.new
+
+  haml :new
+end
+
+post '/new' do
+  item = Item.new(params[:item])
+  if item.save
+    redirect '/'
+  else
+    haml :new
+  end
+end
+
 
